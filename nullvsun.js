c3217fb287d3d8336497320ace41d84367d697e6
Dipraj Howlader <diprajhowlader2015@gmail.com>
2021-02-02 12:18:33 +0600

    nullvsUndefinedPush

---
+++ nullvsun.js
@@ -0,0 +1,22 @@
+let pakhi;
+function add(nm1,nm2){
+    console.log(nm1+nm2);
+    return nm1;
+}
+const result = add(2,5);
+console.log(result);
+
+function add(num1, num2){
+    console.log(num1, num2);
+}
+
+
+
+const premik = {name: "Smart Dude", phone : 65555};
+console.log(premik.gf);
+
+let fun  = undefined;
+console.log(fun);
+
+null 
+
