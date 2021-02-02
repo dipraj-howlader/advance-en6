50bfb356d878fec99067a02062daa6d4f518cbbb
Dipraj Howlader <diprajhowlader2015@gmail.com>
2021-02-02 12:26:16 +0600

    double-vs-triple

---
+++ doublevstriple-equal.js
@@ -0,0 +1,10 @@
+const first = 1;
+const second = true;
+// for == ,it doesnot check any thing, check value only
+// for === ,it will check anything, check value + type;
+if (first === second){
+    console.log('conditon is true');
+}
+else{
+    console.log('condition is false');
+}
