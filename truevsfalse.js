ef8077d9f027ac01feed0fd73082170c297b7456
Dipraj Howlader <diprajhowlader2015@gmail.com>
2021-02-02 11:58:04 +0600

    this is ok

---
+++ truevsfalse.js
@@ -0,0 +1,13 @@
+// const name = 0;
+// const name = ''
+// const name = '0'
+// const name = NaN;
+// const name = [];
+// const name = {};
+const name= ;
+if(name){
+    console.log("true");
+}
+else{
+    console.log("false");
+}
