fcb4fb987990eea37f5283f59a102583d878adba
Dipraj Howlader <diprajhowlader2015@gmail.com>
2021-02-03 00:54:38 +0600

    siktaci

---
+++ scope.js
@@ -0,0 +1,15 @@
+let bonus = 20;
+
+function sum(first, second) {
+    let result = first + second + bonus;
+
+    if (result > 9) {
+       let mood = 'happy';
+        mood = "Feshyy";
+        mood= "funky";
+        mood = "karnki";
+        console.log(mood);
+    }
+}
+const output = sum(3, 7);
+// console.log(output);
